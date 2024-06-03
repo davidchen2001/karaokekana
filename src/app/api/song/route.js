8bb3195e6fbb56667ffd5865b4a1c04af8869f75
David Chen <chuwei.chen2001@gmail.com>
2024-06-03 14:18:14 -0400

    finish API calls

--- src/app/api/song/route.js
+++ src/app/api/song/route.js
@@ -2,6 +2,12 @@ const accessToken = process.env.GENIUS_ACCESS_TOKEN;
 
 import { NextResponse } from "next/server";
 import { getLyrics, getSong } from "genius-lyrics-api";
+import { convert } from "jp-conversion";
+
+import Kuroshiro from "kuroshiro";
+import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
+
+const kuroshiro = new Kuroshiro();
 
 export async function GET(request) {
   const title = request.nextUrl.searchParams.get("title");
@@ -15,7 +21,23 @@ export async function GET(request) {
   };
 
   try {
-    const lyrics = await getLyrics(options);
+    const kanji = await getLyrics(options);
+    await kuroshiro.init(new KuromojiAnalyzer());
+
+    const hiragana = await kuroshiro.convert(kanji, {
+      to: "hiragana",
+    });
+
+    const romaji = await kuroshiro.convert(kanji, {
+      to: "romaji",
+    });
+
+    const lyrics = {
+      kanji: kanji,
+      hiragana: hiragana,
+      romanji: romaji,
+    };
+
     return NextResponse.json({ id: 200, text: lyrics });
   } catch (err) {
     return NextResponse.json({ id: 500, text: err });
