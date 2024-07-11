d9f24630e606870e246f81d9a00700fdbf464e23
David Chen <chuwei.chen2001@gmail.com>
2024-07-10 21:26:52 -0400

    finish form-submission for songs

--- src/app/song/submit/page.js
+++ src/app/song/submit/page.js
@@ -1,7 +1,8 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import Sidebar from "../../sidebar/sidebar";
+import { useForm } from "react-hook-form";
 
 import {
   Input,
@@ -14,6 +15,46 @@ import {
 } from "@chakra-ui/react";
 
 function Submit() {
+  const {
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm();
+
+  const [data, setData] = useState({
+    title: "",
+    artist: "",
+    hiragana: "",
+    romaji: "",
+    kanji: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const onSubmit = () => {
+    const getData = async () => {
+      const response = await fetch(`/api/song`, {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+      return response;
+    };
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        getData().then((response) => {
+          alert("Song has been successfully updated");
+          reset();
+        });
+      }, 3000);
+    });
+  };
+
   return (
     <Box p={4}>
       <Grid gap={6}>
@@ -21,22 +62,56 @@ function Submit() {
           <Sidebar />
         </GridItem>
         <GridItem rowSpan={2} colSpan={2} ml={{ base: 0, md: 60 }} p="4">
-          <VStack spacing={10}>
-            <Input placeholder="song title" />
-            <Input placeholder="song artist" />
-            <Textarea placeholder="hiragana lyrics" />
-            <Textarea placeholder="romaji lyrics" />
-            <Textarea placeholder="kanji lyrics" />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <VStack spacing={10}>
+              <Input
+                name="title"
+                placeholder="song title"
+                value={data.title}
+                onChange={handleChange}
+                isRequired
+              />
+              <Input
+                name="artist"
+                placeholder="song artist"
+                value={data.artist}
+                onChange={handleChange}
+                isRequired
+              />
+              <Textarea
+                name="hiragana"
+                placeholder="hiragana lyrics"
+                value={data.hiragana}
+                onChange={handleChange}
+                isRequired
+              />
+              <Textarea
+                name="romaji"
+                placeholder="romaji lyrics"
+                value={data.romaji}
+                onChange={handleChange}
+                isRequired
+              />
+              <Textarea
+                name="kanji"
+                placeholder="kanji lyrics"
+                value={data.kanji}
+                onChange={handleChange}
+                isRequired
+              />
 
-            <Button
-              borderRadius={0}
-              type="submit"
-              variant="solid"
-              colorScheme="teal"
-            >
-              Submit
-            </Button>
-          </VStack>
+              <Button
+                borderRadius={0}
+                type="submit"
+                variant="solid"
+                colorScheme="teal"
+                isLoading={isSubmitting}
+                isDisabled={isSubmitting}
+              >
+                Submit
+              </Button>
+            </VStack>
+          </form>
         </GridItem>
       </Grid>
     </Box>
