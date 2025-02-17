64791b666e9b9a1a38a4ef06947dd3181bc6373d
David Chen <chuwei.chen2001@gmail.com>
2025-02-17 00:13:12 -0500

    add logging

--- src/lib/db.js
+++ src/lib/db.js
@@ -11,6 +11,7 @@ export async function dbConnect() {
     return global.mongoose.conn;
   } else {
     const conString = process.env.MONGO_URL;
+    console.log(conString);
 
     const promise = mongoose.connect(conString, {
       autoIndex: true,
